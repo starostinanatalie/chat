0693f914c2dcd7be817501794bac3a6d57eaeded
starostinanatalie <starostina.natalie@gmail.com>
2024-01-08 22:26:18 +0300

    13 lesson

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,4 @@
-
+const name = document.getElementById('name');
 const checkSpam = (str) => {
     const addStr = str.toUpperCase();
     const indexV = addStr.search('VIAGRA');
@@ -16,10 +16,16 @@ const checkSpam = (str) => {
 }
 
 const addNewPost = () => {
-    let name = document.getElementById('name');
-    const nameValue = name.value;
+    let nameValue = name.value;
+    if (nameValue === "") {
+        nameValue = "username";
+    }
     let avatar = document.getElementById('avatar');
-    const avatarValue = avatar.value;
+    let avatarValue = avatar.value;
+    if (avatarValue === "") {
+        let random = Math.round(Math.random() * 6);
+        avatarValue = `./assets/images/img${random}.jpg`;
+    }
     let str = document.getElementById('textComment');
     const strValue = str.value;
     const text = checkSpam(strValue);
@@ -27,14 +33,19 @@ const addNewPost = () => {
 
     let newDiv = document.createElement('div');
     newDiv.classList.add('chatItem');
-    let chatName = document.createElement('h3');
-    chatName.classList.add('chatName');
-    chatName.textContent = nameValue;
-    newDiv.append(chatName);
     let chatAvatar = document.createElement('img');
     chatAvatar.classList.add('chatAvatar');
     chatAvatar.setAttribute('src', avatarValue);
     newDiv.append(chatAvatar);
+    let chatName = document.createElement('h3');
+    chatName.classList.add('chatName');
+    chatName.textContent = nameValue;
+    newDiv.append(chatName);
+    let chatDate = document.createElement('h4');
+    chatDate.classList.add('chatDate');
+    let date = new Date();
+    chatDate.textContent = date.toDateString() + date.toLocaleTimeString();
+    newDiv.append(chatDate);
     let chatText = document.createElement('p');
     chatText.classList.add('chatText');
     chatText.textContent = text;
@@ -45,9 +56,24 @@ const addNewPost = () => {
     name.value = '';
     avatar.value = '';
     str.value = '';
-
-    
 }
+
+const nameDecision = () => {
+    const labelForName = document.getElementById('labelForName');
+    if (no.checked) {
+        name.classList.add('invisible');
+        labelForName.classList.add('invisible');
+    } else {
+        name.classList.remove('invisible');
+        labelForName.classList.remove('invisible');
+    }
+}
+
+const yes = document.getElementById('yes');
+const no = document.getElementById('no');
+yes.addEventListener('change', nameDecision);
+no.addEventListener('change', nameDecision);
+
 
 const button = document.getElementById('button');
 button.addEventListener('click', addNewPost);
